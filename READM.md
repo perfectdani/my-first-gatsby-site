9b590c5c861a8ffaa259590410e354799b8c30a1
perfectdani <ooodeve@gmail.com>
2022-01-19 18:22:12 +0200

    Finished Gatsby Tutorial Part 2

--- READM.md
+++
@@ -1,1 +0,0 @@
-"# my-first-gatsby-site" 
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const AboutPage = () => {
+    return(
+        <Layout pageTitle = "About">
+            <p>This is about page.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
